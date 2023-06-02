386de5043ee9ad58c8cd63c4be04e9f441df58f5
zlept55 <rochinhas.pais@gmail.com>
2023-06-02 16:27:55 +0000

    JS TREINO

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+console.log(fullName[2]);
+
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName)
+
+let index = email.indexOf('@');
+console.log(index);
